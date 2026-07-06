8d0527c5ee75c2ffa1ba61b5f5322c4e87ec55ed
Alina Peng <alinapeng@Alinas-MacBook-Air.local>
2026-07-06 11:43:05 -0400

    Update controller comments

---
+++ controllers/products.js
@@ -0,0 +1,24 @@
+// Create function (createProduct)
+export const createProduct = async (req, res) => {
+
+};
+
+// Read All function (getProducts)
+export const getProducts = async (req, res) => {
+
+};
+
+// Read One function (getProductById)
+export const getProductById = async (req, res) => {
+
+};
+
+// Update function (updateProduct)
+export const updateProduct = async (req, res) => {
+
+};
+
+// Delete function (deleteProduct)
+export const deleteProduct = async (req, res) => {
+
+};
