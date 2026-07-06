f5df84b0dfff5c22f4d9dfd3aea57de3bc59173e
PavelAnnor <annorpavel@gmail.com>
2026-07-06 13:17:41 -0400

    idk

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,22 @@
-import Product from "./models/product.js"
+import Product from "../models/product.js";
 
+// Create function (createProduct)
+export async function createListing(req, res) {
+    const result = await Product.find({}).limit(10)
+    res.json(result)
+}
 
+// Read All function (getProducts)
+export async function getListings(req, res) {}
 
+// Read One function (getProductById)
+export async function getProductById(req, res) {}
 
+// Update function (updateProduct)
+export async function updateProduct(req, res) {}
+
+// Delete function (deleteProduct)
+export async function deleteProduct(req, res) {
+  const result = await Product.findByIdAndDelete(req.params.id);
+  res.json(result);
+}
